d75c399d962d743c8c129dcf07c3a41449eb9e15
vishal5251 <vishaljaiswal9551@gmail.com>
2023-04-12 18:53:03 +0530

    changes

--- src/Components/Profile/InviteAdmin.jsx
+++ src/Components/Profile/InviteAdmin.jsx
@@ -21,7 +21,7 @@ function InviteAdmin({ mode }) {
   async function handleInvite() {
     try {
       const res = await axios.post(
-        "http://localhost:8080/api/v1/team/inviteteam",
+        "https://bdmhrmnode.bigbros.link/api/v1/team/inviteteam",
         {
           email,
           role,
